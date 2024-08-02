89d13801f1bafdff43c43735565a9b09665a13be
shahadAliz <cs.shahadz@gmail.com>
2024-08-02 20:11:34 +0300

    first commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+
+
+var grad = 50
+
+if (grad >= 90 ){
+    print("Excellent")
+    
+}else if (grad >= 80 ){
+    print("Very Good")
+    
+}else if (grad >= 70){
+    print("Good")
+}
+else if (grad >= 60){
+    print("Pass")
+}
+else if (grad < 60){
+    print("Fail")
+}
+
